c67775aeb9528c1803dab0da0c26f5af3313b5f0
Desiree <chuchu930716@gmail.com>
2024-02-21 20:55:27 -0800

    added some contents and copied to the side branch

---
+++ froyo.js
@@ -0,0 +1,31 @@
+// Prompt the user for a list of integers separated by commas.
+const flavorsInput = prompt(
+    "Please enter the flavors of the froyo separated by commas without spaces.",
+    ""
+  );
+
+  
+// Split the string of numbers into an array of strings.
+const flavorsArray = flavorsInput.split(",");
+
+// Convert the array of strings into an array of flavorCount.
+const flavorCount = [];
+for (const flavor of flavorsArray) {
+    if {
+        
+    } else {
+        
+    }
+}
+
+
+// Results in the console
+console.log("Numbers of Froyo Flavors:", flavorCount);
+
+
+// to delete
+//     let i = 0; i < stringArray.length; i++) {
+//   const str = stringArray[i];
+//   const number = parseInt(str);
+//   numbers.push(number);
+// }
